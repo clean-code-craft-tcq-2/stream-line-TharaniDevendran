0c63ab5980d775cc6e3650a497f9c0ec270751a6
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-04-07 10:09:51 +0530

    Update BatteryReadingTest.cpp

--- BatteryReadingTest.cpp
+++ BatteryReadingTest.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-
+#include BatteryReadingSender.h
 #include "catch.hpp"
 
 TEST_CASE("Tests to check if data is correctly read from file") {
@@ -8,7 +8,7 @@ TEST_CASE("Tests to check if data is correctly read from file") {
   float ChargeRate[NO_OF_READINGS] = {0};
   ReadBatterReadingsfromFile(Temperature,StateOfCharge,ChargeRate);
   
-  float expectedoutput[3][3] = {{49 66 0.34}, {28 67 0.8},{88 90 0.52}};
+  float expectedoutput[3][3] = {{49,66,0.34}, {28,67,0.8},{88,90,0.52}};
   for(int i=0;i<3;i++)
   {
     REQUIRE(Temperature[i] == expectedoutput[i][0]);
