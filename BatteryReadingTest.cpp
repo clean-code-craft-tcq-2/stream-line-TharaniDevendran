1fc5fd7cb1d841f4dd416c2754a0ef3685736be3
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-04-07 10:06:25 +0530

    Update BatteryReadingTest.cpp

--- BatteryReadingTest.cpp
+++ BatteryReadingTest.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "test/catch.hpp"
+#include "catch.hpp"
 
 TEST_CASE("Tests to check if data is correctly read from file") {
   float Temperature[NO_OF_READINGS] = {0};
