d8ff81fc59aecf1e38b1f021909a48160886be51
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-04-12 17:32:20 +0530

    Update BatteryReadingReceiver.c

--- BatteryReadingReceiver.c
+++ BatteryReadingReceiver.c
@@ -1,1 +1,18 @@
 #include "BatteryReadingReceiver.h"
+#include "BatteryReadingSender.h"
+
+void GetSensorDataFromConsole(float* Temperature, float* SOC, float* ChargeRate)
+{
+  int readingIndex = 0;
+  
+  for(readingIndex = 0; readingIndex < NO_OF_READINGS; readingIndex++)
+  {
+    scanf("%f,%f,%f",&Temperature[readingIndex],&SOC[readingIndex],&ChargeRate[readingIndex]);
+  }
+}
+
+void BMSReceiver()
+{
+  float Temperature[NO_OF_READINGS],SOC[NO_OF_READINGS],ChargeRate[NO_OF_READINGS];
+  GetSensorDataFromConsole(&Temperature[0],&SOC[0],&ChargeRate[0]);
+}
