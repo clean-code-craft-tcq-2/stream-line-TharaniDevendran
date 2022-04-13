200772aea42d7193c07d92a65d35036326c515bd
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-04-13 07:57:54 +0530

    Update test_BatteryReadingReceiver.cpp

--- test_BatteryReadingReceiver.cpp
+++ test_BatteryReadingReceiver.cpp
@@ -5,16 +5,30 @@
 #include "BatteryReadingSender.h"
 
 TEST_CASE("Tests to check whether sensor data is read from console") {
-  float test_Temperature[NO_OF_READINGS] = {0};
-  float test_SOC[NO_OF_READINGS] = {0};
-  float test_ChargeRate[NO_OF_READINGS] = {0};
-  GetSensorDataFromConsole(&test_Temperature[0],&test_SOC[0],&test_ChargeRate[0]);
+  float Temperature[NO_OF_READINGS], SOC[NO_OF_READINGS], ChargeRate[NO_OF_READINGS];
+  float test_Temperature[NO_OF_READINGS], test_SOC[NO_OF_READINGS], test_ChargeRate[NO_OF_READINGS];
+  float temp,soc,chrRate;
+  int index;
   
-  float expectedoutput[2][3] = {{49,66,0.34}, {28,67,0.8}};
-  for(int i=0;i<2;i++)
+  //Call receiver to read sensor data
+  GetSensorDataFromConsole(&Temperature[0],&SOC[0],&ChargeRate[0]);
+  
+  FILE *test_SensorData_fp = fopen("./Battery_Reading.txt","r");
+  
+  for(index = 0; fscanf(test_SensorData_fp, "%f %f %f\n", &temp, &soc, &chrRate)!=EOF; index++)
   {
-    REQUIRE(test_Temperature[i] == expectedoutput[i][0]);
-    REQUIRE(test_SOC[i] == expectedoutput[i][1]);
-    REQUIRE(test_ChargeRate[i] == expectedoutput[i][2]);
+      test_Temperature[index] = temp;
+      test_SOC[index] = soc;
+      test_ChargeRate[index] = chrRate;
   }
+  
+  //Verify that readings are read from input file
+  for(index = 0; index < NO_OF_READINGS; index++)
+  {
+    REQUIRE(Temperature[index] == test_Temperature[index]);
+    REQUIRE(SOC[index] == test_SOC[index]);
+    REQUIRE(ChargeRate[index] == test_ChargeRate[index]);
+  }
+  
+  fclose(test_SensorData_fp);
 }
