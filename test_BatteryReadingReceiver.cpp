0a8fa4d3d3ff904979ec2ebadaf85b07f580a045
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-04-13 08:36:44 +0530

    Update test_BatteryReadingReceiver.cpp

--- test_BatteryReadingReceiver.cpp
+++ test_BatteryReadingReceiver.cpp
@@ -8,7 +8,7 @@ TEST_CASE("Tests to check whether sensor data is read from console") {
   float test_Temperature[NO_OF_READINGS] = {0};
   float test_SOC[NO_OF_READINGS] = {0};
   float test_ChargeRate[NO_OF_READINGS] = {0};
-  ReadBatterReadingsfromFile(&test_Temperature[0],&test_SOC[0],&test_ChargeRate[0]);
+  GetSensorDataFromConsole(&test_Temperature[0],&test_SOC[0],&test_ChargeRate[0]);
   
   float expectedoutput[2][3] = {{49,66,0.34}, {28,67,0.8}};
   for(int i=0;i<2;i++)
