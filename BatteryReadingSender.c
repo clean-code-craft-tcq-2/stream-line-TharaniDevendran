4783988f639db4f8a15a11fdc75d647ca1390698
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-04-15 17:04:51 +0530

    Update BatteryReadingSender.c

--- BatteryReadingSender.c
+++ BatteryReadingSender.c
@@ -3,8 +3,13 @@
 #include <math.h>
 #include <stdlib.h>
 
-void printOnConsole(float temperature, float SOC, float CR){
-    printf(" Temperature : %.2f , State of Charge : %.2f , Charge Rate : %.2f \n",temperature,SOC,CR);
+void printOnConsole(float* Temperature, float* SOC, float* ChargeRate)
+{
+        for(int i = 0; i<NO_OF_READINGS;i++)
+    {
+        printf(" Temperature : %.2f , State of Charge : %.2f , Charge Rate : %.2f \n",Temperature[i],SOC[i],ChargeRate[i]);
+    }
+  
 }
 
 float* ReadBatterReadingsfromFile(float* Temperature, float* SOC, float* ChargeRate)
@@ -25,8 +30,7 @@ float* ReadBatterReadingsfromFile(float* Temperature, float* SOC, float* ChargeR
             *(ChargeRate+i)   = CR_reading;
         }
     }
-   return Temperature;
-    //fclose(fp);  
+    fclose(fp);  
 }
 
 void SendBatteryReadingsToConsole(float* Temperature, float* SOC, float* ChargeRate)
@@ -37,7 +41,7 @@ void SendBatteryReadingsToConsole(float* Temperature, float* SOC, float* ChargeR
         Temperature_ToPrint = *(Temperature+i);
         SOC_ToPrint = *(SOC+i);
         CR_ToPrint = *(ChargeRate+i);
-        printOnConsole(Temperature_ToPrint, SOC_ToPrint, CR_ToPrint);
+        //printOnConsole(Temperature_ToPrint, SOC_ToPrint, CR_ToPrint);
     }
 }
 
@@ -45,5 +49,6 @@ void ReadBatterReadingsfromFile()
 {
   float Temperature[NO_OF_READINGS], SOC[NO_OF_READINGS], ChargeRate[NO_OF_READINGS] = {0};
   ReadBatterReadingsfromFile(Temperature,SOC,ChargeRate);
-  SendBatteryReadingsToConsole(Temperature,SOC,ChargeRate);
+  printOnConsole(Temperature,SOC,ChargeRate);
+  //SendBatteryReadingsToConsole(Temperature,SOC,ChargeRate);
 }
