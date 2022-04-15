93ac7ab8740652460c09d11acaea11e058c9747e
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-04-15 17:24:32 +0530

    Update BatteryReadingTest.cpp

--- BatteryReadingTest.cpp
+++ BatteryReadingTest.cpp
@@ -6,7 +6,7 @@ TEST_CASE("Tests to check if data is correctly read from file") {
   float Temperature[NO_OF_READINGS] = {0};
   float SOC[NO_OF_READINGS] = {0};
   float ChargeRate[NO_OF_READINGS] = {0};
-  BatteryReadingSender();
+  BatteryReadingSender(Temperature,SOC,ChargeRate);
   
   float expectedoutput[3][3] = {{49,66,0.34}, {28,67,0.8},{88,90,0.52}};
   for(int i=0;i<3;i++)
