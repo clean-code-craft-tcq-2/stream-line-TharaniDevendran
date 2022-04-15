79e411a33dd9ba4c02baeb41a855bf607241df11
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-04-15 17:24:35 +0530

    Update BatteryReadingSender.h

--- BatteryReadingSender.h
+++ BatteryReadingSender.h
@@ -3,4 +3,4 @@
 void printOnConsole(float temperature, float SOC, float CR);
 void ReadBatteryReadingsfromFile(float* Temperature, float* SOC, float* ChargeRate);
 void SendBatteryReadingsToConsole(float* Temperature, float* SOC, float* ChargeRate);
-void BatteryReadingSender();
+void BatteryReadingSender(float* Temperature, float* SOC, float* ChargeRate);
