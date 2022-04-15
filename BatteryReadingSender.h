9341e999f8779d0d3a7335c89d99598347cd84d8
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-04-15 17:17:10 +0530

    Update BatteryReadingSender.h

--- BatteryReadingSender.h
+++ BatteryReadingSender.h
@@ -1,6 +1,6 @@
 #define NO_OF_READINGS 50
 
 void printOnConsole(float temperature, float SOC, float CR);
-float* ReadBatterReadingsfromFile(float* Temperature, float* SOC, float* ChargeRate);
+void ReadBatteryReadingsfromFile(float* Temperature, float* SOC, float* ChargeRate);
 void SendBatteryReadingsToConsole(float* Temperature, float* SOC, float* ChargeRate);
-void BatterReadingSender();
+void BatteryReadingSender();
