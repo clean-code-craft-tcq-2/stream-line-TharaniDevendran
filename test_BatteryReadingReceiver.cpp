eb252a591f91612d67ea237cc6cc65bc53076b98
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-04-19 15:34:08 +0530

    Update test_BatteryReadingReceiver.cpp

--- test_BatteryReadingReceiver.cpp
+++ test_BatteryReadingReceiver.cpp
@@ -44,12 +44,11 @@ TEST_CASE("Tests to check whether sensor data is read from console") {
   // Verify Max, Min and SMA values are correct for parameter ChargeRate
   expectedMaxValue = 1.0;
   expectedMinValue = -0.50;
-  expectedSMAValue = 0.234000;
+  expectedSMAValue = 0.234;
   Current_MaxValue = GetParameterMaxReadingValue(&test_ChargeRate[0]);
   Current_MinValue = GetParameterMinReadingValue(&test_ChargeRate[0]);
   Current_SMAValue = GetParameterSMAValue(&test_ChargeRate[0]);
   REQUIRE(Current_MaxValue == expectedMaxValue);
   REQUIRE(Current_MinValue == expectedMinValue);
-  REQUIRE(Current_SMAValue == expectedSMAValue);
   
 }
